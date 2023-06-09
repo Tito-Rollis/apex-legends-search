8925f77e4062397a849679f3c650e04070afe354
Tito-Rollis <ouvalres@gmail.com>
2023-06-09 10:22:21 +0700

    initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav className="w-auto bg-red-600 h-fit py-3">
+                <img className="mx-auto" src="/icons/apex-white-icon.svg" alt="" />
+            </nav>
+        </div>
+    );
 }
 
 export default App;
